c80b761c6a2bc1a965551d2224c7a65683c34147
Denis Polyakov <p-ov.da@ya.ru>
2022-04-28 21:58:52 +0300

    Show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='pokemons', null=True)
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='pokemons', blank=True)
 
     def __str__(self):
         return f'{self.title}'
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,10 @@
 import folium
 import json
 
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpRequest
 from django.shortcuts import render
+from .models import Pokemon
+
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -40,11 +42,18 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
+    pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+        if pokemon.image:
+            pokemons_on_page.append({
+                'pokemon_id': pokemon.id,
+                'title_ru': pokemon.title,
+                'img_url':pokemon.image.url,
+            })
+        else:
+            pokemons_on_page.append({
+            'pokemon_id': pokemon.id,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
