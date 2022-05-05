74b4e3ba8d3efb4810d81f22ba82e18ef6b86357
Denis Polyakov <p-ov.da@ya.ru>
2022-05-05 10:19:31 +0300

    Filtered by date

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,10 +1,11 @@
-import folium
 import json
 
-from django.http import HttpResponseNotFound, HttpRequest
+import folium
+from django.http import HttpRequest, HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon, PokemonEntity
+from django.utils import timezone
 
+from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -28,10 +29,11 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    now = timezone.localtime()
+    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+    #     pokemons = json.load(database)['pokemons']
 
-    pokemons_entity = PokemonEntity.objects.all()
+    pokemons_entity = PokemonEntity.objects.filter(appeared_at__date__lte=now, disappeared_at__date__gte=now)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemons_entity:
