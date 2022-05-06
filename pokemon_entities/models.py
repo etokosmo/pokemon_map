d392c3de75daf9b682ce7c940943650213602222
Denis Polyakov <p-ov.da@ya.ru>
2022-05-06 23:42:05 +0300

    PEP8 and add queryset method

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,11 +4,16 @@ from django.db import models
 class Pokemon(models.Model):
     """Покемон"""
     title = models.CharField(verbose_name='наименование', max_length=200)
-    title_en = models.CharField(verbose_name='наименование на английском', max_length=200, blank=True)
-    title_jp = models.CharField(verbose_name='наименование на японском', max_length=200, blank=True)
+    title_en = models.CharField(
+        verbose_name='наименование на английском', max_length=200, blank=True)
+    title_jp = models.CharField(
+        verbose_name='наименование на японском', max_length=200, blank=True)
     description = models.TextField(verbose_name='описание', blank=True)
-    image = models.ImageField(verbose_name='фотография', upload_to='pokemons', null=True, blank=True)
-    previous_evolution = models.ForeignKey("self", related_name="next_evolutions", verbose_name='предыдущая эволюция', on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(
+        verbose_name='фотография', upload_to='pokemons', null=True, blank=True)
+    previous_evolution = models.ForeignKey("self", related_name="next_evolutions",
+                                           verbose_name='предыдущая эволюция', on_delete=models.SET_NULL, null=True,
+                                           blank=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -16,16 +21,21 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     """Характеристики Покемона"""
-    pokemon = models.ForeignKey(Pokemon, verbose_name='покемон', on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon, verbose_name='покемон', on_delete=models.CASCADE)
     lat = models.FloatField(verbose_name='широта')
     lon = models.FloatField(verbose_name='долгота')
-    appeared_at = models.DateTimeField(verbose_name='когда появился', null=True, blank=True)
-    disappeared_at = models.DateTimeField(verbose_name='когда исчезнет', null=True, blank=True)
+    appeared_at = models.DateTimeField(
+        verbose_name='когда появился', null=True, blank=True)
+    disappeared_at = models.DateTimeField(
+        verbose_name='когда исчезнет', null=True, blank=True)
     level = models.IntegerField(verbose_name='уровень', null=True, blank=True)
-    health = models.IntegerField(verbose_name='здоровье', null=True, blank=True)
+    health = models.IntegerField(
+        verbose_name='здоровье', null=True, blank=True)
     strength = models.IntegerField(verbose_name='сила', null=True, blank=True)
     defence = models.IntegerField(verbose_name='защита', null=True, blank=True)
-    stamina = models.IntegerField(verbose_name='выносливость', null=True, blank=True)
+    stamina = models.IntegerField(
+        verbose_name='выносливость', null=True, blank=True)
 
     def __str__(self):
         return f"{self.pokemon.title}: {self.lat}, {self.lon}"
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -73,7 +73,7 @@ def get_next_evolution(request, requested_pokemon: Pokemon) -> Optional[dict]:
     """Получаем характиристики покемона в кого эволюционируем"""
     if not requested_pokemon.next_evolutions.all():
         return None
-    pokemon = requested_pokemon.next_evolutions.all()[0]
+    pokemon = requested_pokemon.next_evolutions.all().first()
     return {
         "title_ru": pokemon.title,
         "pokemon_id": pokemon.id,
